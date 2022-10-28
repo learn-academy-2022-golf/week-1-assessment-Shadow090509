ff37e4743c7fb5938e4df719651128f05573ed0e
Shadow090509 <jamoso090509@gmail.com>
2022-10-28 16:13:31 -0700

    still working on this

--- coding-concepts.js
+++ coding-concepts.js
@@ -6,49 +6,49 @@
 // Then, uncomment the console.log() and verify the output. Briefly explain why your initial answer was correct or incorrect.
 
 // --------------------INSTRUCTOR EXAMPLE: What will this log?
-const colors = ["tangerine", "magenta", "lilac", "daffodil"]
-// console.log(colors.push("indigo"))
+const colors = ["tangerine", "magenta", "lilac", "daffodil"];
+console.log(colors.push("indigo"));
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: 5
+// b) Verify and explain: output of the method will provide the length of the new array.
 
 // --------------------1) What will this log?
 
-const cohort = "LEARN 2022"
-// console.log(cohort.length)
+const cohort = "LEARN 2022";
+console.log(cohort.length);
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: 10
+// b) Verify and explain: .length is set to return the number of elements in that array.
 
 // --------------------2) What will this log?
 
-const greeting = "Hello World!"
-// console.log(greeting[4])
+const greeting = "Hello World!";
+console.log(greeting[4]);
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: o
+// b) Verify and explain: the string method console.log(greeting[4] will reference the single character from the string by the index provided)
 
 // --------------------3) What will this log?
 
-const languages = ["JavaScript", "Ruby", "Python", "C++"]
-const index = 1
-// console.log(languages[index])
+const languages = ["JavaScript", "Ruby", "Python", "C++"];
+const index = 1;
+console.log(languages[index]);
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Ruby
+// b) Verify and explain: console.log(languages[]) will reference what is provided in the index which is in this case, 1.
 
 // --------------------4) What will this log?
 
-const weekendDays = ["saturday", "sunday"]
-// console.log(weekendDays.toUpperCase())
+const weekendDays = ["saturday", "sunday"];
+//console.log(weekendDays.toUpperCase());
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: SATURDAY SUNDAY
+// b) Verify and explain: error messege saying that .toUpperCase() is not a function. I'm not sure why this is
 
 // --------------------5) What will this log?
 
-const dataTypes = ["number", "string", "Boolean", "undefined"]
-// console.log(typeof dataTypes.length)
+const dataTypes = ["number", "string", "Boolean", "undefined"];
+console.log(typeof dataTypes.length);
 
 // a) Your answer:
 // b) Verify and explain:
--- interview-questions.md
+++ interview-questions.md
@@ -8,33 +8,35 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 INSTRUCTOR EXAMPLE: What is a conditional statement?
 
-Your answer:
+Your answer: a decision tree
 
-Researched answer:
+Researched answer: commands for handling decisions
 
 1. What is git? What is the difference between git and Github?
 
-Your answer:
+Your answer: global information tracker.
+git is on our computers.
+Github is located in Idaho
 
-Researched answer:
+Researched answer: created in 2005 by Linus Torvalds to track changes in source code.
 
 2. Which JavaScript operators will return a Boolean value?
 
-Your answer:
+Your answer: ==
 
-Researched answer:
+Researched answer: (loose equality ==), (strict equality ===), (greater than <), (less than >), (greater than or equal to >=), (less than or equal to <=), (and &&), (or ||), (negation !), (!==, !())
 
 3. What is an index? What is the difference between index and value?
 
-Your answer:
+Your answer: a data type
 
-Researched answer:
+Researched answer: the index is the position of a regular expression within a string.
 
 4. Compare and contrast arrays and strings. Please include ways in which they are the same and ways in which they are different.
 
-Your answer:
+Your answer: a string is everything on one index. an array is everything in its own index.
 
-Researched answer:
+Researched answer: a string is a collection of characters. an array holds a collection of elements with the same types of data.
 
 5. Give a brief description of proper pair programming techniques. What are the roles of each person?
 
